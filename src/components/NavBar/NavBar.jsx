02bffc8fb3c568c9f579c92f40a25ca87e0f822f
AlfredSM <ingalfresm@gmail.com>
2022-12-29 16:22:11 -0600

    Entrega proyecto final

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
+import Logo from '../../Logo.png'
 import { CartWidget } from '../CartWidget/CartWidget'
 import { Link } from 'react-router-dom'
-import Logo from '../img/Logo.png'
 import './NavBar.css'
 
 
